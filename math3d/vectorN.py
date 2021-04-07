9448336a048d9e4b70ac0910b3bb9f3e165ed3c8
Jeremie Passerin <passerin@blur.com>
2021-04-07 16:41:17 -0700

    Fix to return a Vector3 when doing cross product

--- math3d/vectorN.py
+++ math3d/vectorN.py
@@ -492,7 +492,7 @@ class VectorNArray(ArrayBase):
         vec1 = (pos1 - centers).normal()
         vec2 = (pos2 - centers).normal()
 
-        ret = np.cross(vec1, vec2)
+        ret = vec1.cross(vec2)
 
         if fallback:
             # Now that we have the normals calculated, we figure out how to fall back
